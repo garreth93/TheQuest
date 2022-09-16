baae0ea72e0ed106ba74e9c9de8f6c5eefbf5663
Sam <nerod18@gmail.com>
2022-09-16 12:55:10 +0200

    Creacion del archivo records y clase para gestion de BBDD

---
+++ thequest/records.py
@@ -0,0 +1,64 @@
+import sqlite3
+
+
+class AministraDB:
+    def __init__(self, ruta):
+        self.ruta = ruta
+
+    def leeRecords(self):
+        '''Este metodo se encarga de consultar los datos guardados
+        en la base de datos.'''
+
+        # Consulta a realizar
+        consulta = "SELECT * FROM records ORDER BY puntos DESC LIMIT 5"
+
+        # Conecta con la base de datos
+        conexion = sqlite3.connect(self.ruta)
+
+        # Crear en cursor de consulta
+        cursor = conexion.cursor()
+
+        # Transmitir la consulta para ejecutarla en SQL
+        cursor.execute(consulta)
+
+        records = []
+        nombres_columnas = []
+
+        for desc_columna in cursor.description:
+            nombres_columnas.append(desc_columna[0])
+
+        datos = cursor.fetchall()
+        for dato in datos:
+            record = {}
+            indice = 0
+            for nombre in nombres_columnas:
+                record[nombre] = dato [indice]
+                indice += 1
+            records.append(record)
+
+        conexion.close()
+
+        return records
+
+    def almacenaRecord(self, nombre, puntos):
+        '''Este metodo sirve para poder guardar un nuevo record
+        en la base de datos'''
+
+        # Consulta a realizar para introducir el nuevo record
+        consulta = "INSERT INTO records (nombre,puntos) VALUES (?,?)"
+        
+        # Conexion con la base de datos
+        conexion = sqlite3.connect(self.ruta)
+        
+        # Cursor
+        cursor = conexion.cursor()
+
+        # Ejecucion de la consulta
+        cursor.execute(consulta, (nombre, puntos))
+
+        # Realizacion del commit para guardar
+        conexion.commit()
+        
+        # Cerrar conexion
+        conexion.close()
+
